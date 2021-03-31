bc825f1d5e455d84480ba8682e4ca1796bf00feb
JohanLeonardoIbarra <johanleonardois@ufps.edu.co>
2021-03-30 19:26:09 -0500

    creado metodo main

--- src/manejo/de/eventos/ManejoDeEventos.java
+++ src/manejo/de/eventos/ManejoDeEventos.java
@@ -15,7 +15,8 @@ public class ManejoDeEventos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Gui gui = new Gui();
+        gui.setVisible(true);
     }
     
 }
